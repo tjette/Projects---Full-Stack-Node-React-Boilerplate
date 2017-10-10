b1dd84dab0ebb1ff74fb122bad5b30d543cc720f
davisgd <davis.diede@gmail.com>
2017-10-10 14:25:45 -0700

    Added menu styling.

--- client/src/structure/header/Header.js
+++ client/src/structure/header/Header.js
@@ -1,40 +1,50 @@
 import React from 'react'
 import NavItem from './NavItem'
 import PropTypes from 'prop-types'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Button from 'material-ui/Button'
+import injectSheet from 'react-jss'
 
 const propTypes = {
   userData: PropTypes.object.isRequired
 }
 
 const styles = {
-  header: {
+  root: {
     top: 0,
     left: 0,
     right: 0,
     height: 50,
     backgroundColor: 'gray',
     display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-around',
     fontFamily: 'Gloria Hallelujah',
     color: 'black'
   }
 }
 
+const enhancer = injectSheet(styles)
+
 const Header = (props) => {
   return (
     <header>
-      <nav style={styles.header}>
-        <NavItem to='/'>Home</NavItem>
-        <NavItem to='/about'>About</NavItem>
-        <NavItem to='/jobs'>Jobs</NavItem>
-        { !props.userData.user ? <NavItem to='/signup'>Sign Up</NavItem> : <NavItem to='/' onClick={() => props.userData.logoutUser()}>Log Out</NavItem> }
-        { props.userData.user ? <NavItem to='/profile'>Profile</NavItem>
-          : <NavItem to='/login'>Login</NavItem>
-        }
+      <nav>
+        <AppBar classes={{root: props.classes.root}} position='static'>
+          <NavItem to='/'>Home</NavItem>
+          <NavItem to='/about'>About</NavItem>
+          <NavItem to='/jobs'>Jobs</NavItem>
+          { !props.userData.user ? <NavItem to='/signup'>Sign Up</NavItem> : <NavItem to='/' onClick={() => props.userData.logoutUser()}>Log Out</NavItem> }
+          { props.userData.user ? <NavItem to='/profile'>Profile</NavItem>
+            : <NavItem to='/login'>Login</NavItem>
+          }
+        </AppBar>
       </nav>
     </header>
   )
 }
 Header.propTypes = propTypes
 
-export default Header
+export default enhancer(Header)
