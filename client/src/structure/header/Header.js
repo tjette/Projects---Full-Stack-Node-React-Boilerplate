23e4d265788432c0ff38727aa35a0e90e7757810
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-10-15 21:05:48 -0600

    arranged the navbar so logout button is on the far right now

--- client/src/structure/header/Header.js
+++ client/src/structure/header/Header.js
@@ -33,10 +33,9 @@ const Header = (props) => {
         <NavItem to='/'>Home</NavItem>
         <NavItem to='/about'>About</NavItem>
         {props.userData.user ? <NavItem to='/jobs'>Jobs</NavItem> : null }
+        { props.userData.user ? <NavItem to='/profile'>Profile</NavItem> : <NavItem to='/login'>Login</NavItem>}
         { !props.userData.user ? <NavItem to='/signup'>Sign Up</NavItem> : <a href='/' onClick={props.userData.logoutUser}>Log Out</a> }
-        { props.userData.user ? <NavItem to='/profile'>Profile</NavItem>
-          : <NavItem to='/login'>Login</NavItem>
-        }
+
       </nav>
     </header>
   )
