4981855a9cea4b62f2b1d54eb1e92b84e59e8462
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-10-16 16:21:27 -0600

    adding images instead of just color on home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,44 +16,58 @@ const styles = {
   header: {
     textAlign: 'center'
   },
-  containerItems: {
+  jobSeekerContainer: {
+    display: 'flex',
+    width: '33%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'url(https://s3-us-west-1.amazonaws.com/prosky/careerbuzz/articles/image_prevs/000/000/334/normal/t6njoebtarec-lee-campbell.jpg?1491887042) no-repeat',
+    jobSeekerItems: {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      '&:hover': {
+        opacity: 0.7
+      }
+    }
+  },
+  employerContainer: {
+    display: 'flex',
+    width: '33%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'url(https://resumer.io/images/blog/hiring.jpg) no-repeat',
+    employerItems: {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      '&:hover': {
+        opacity: 0.7
+      }
+    }
+
+  },
+  jobContainer: {
     display: 'flex',
     width: '33%',
     justifyContent: 'center',
-    alignItems: 'center'
-
-  },
-  jobSeeker: {
-    backgroundColor: 'green',
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
     alignItems: 'center',
-    '&:hover': {
-      backgroundColor: 'lightgreen'
-    }
-  },
-  employer: {
-    backgroundColor: 'red',
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '&:hover': {
-      backgroundColor: 'coral'
-    }
-  },
-  jobs: {
-    backgroundColor: 'blue',
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '&:hover': {
-      backgroundColor: 'lightblue'
+    background: 'url(http://www.bizmagsb.com/wp-content/uploads/2015/10/tech.jpg) no-repeat',
+    jobItems: {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      '&:hover': {
+        opacity: 0.7
+      }
     }
   },
   link: {
@@ -71,14 +85,14 @@ const enhancer = injectSheet(styles)
 const Home = (props) => {
   return (
     <div className={props.classes.container}>
-      <div className={props.classes.containerItems}>
-        <Button raised className={props.classes.jobSeeker}><Link className={props.classes.link} to='/signup/job-seeker'>Job Seeker</Link></Button>
+      <div className={props.classes.jobSeekerContainer}>
+        <Button raised className={props.classes.jobSeekerContainer.jobSeekerItems}><Link className={props.classes.link} to='/signup/job-seeker'>Job Seeker</Link></Button>
       </div>
-      <div className={props.classes.containerItems}>
-        <Button raised className={props.classes.employer}><Link className={props.classes.link} to='/signup/employer'>Employer</Link></Button>
+      <div className={props.classes.employerContainer}>
+        <Button raised className={props.classes.employerContainer.employerItems}><Link className={props.classes.link} to='/signup/employer'>Employer</Link></Button>
       </div>
-      <div className={props.classes.containerItems}>
-        <Button raised className={props.classes.jobs}><Link className={props.classes.link} to='/jobs'>Jobs</Link></Button>
+      <div className={props.classes.jobContainer}>
+        <Button raised className={props.classes.jobContainer.jobItems}><Link className={props.classes.link} to='/jobs'>Jobs</Link></Button>
       </div>
     </div>
   )
