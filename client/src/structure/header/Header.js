7e97bcf9c4cd635cd0f6598838eb0d4a91e696d2
davisgd <davis.diede@gmail.com>
2017-10-17 12:43:31 -0700

    Added styling that doesn't really work good.

--- client/src/structure/header/Header.js
+++ client/src/structure/header/Header.js
@@ -3,6 +3,8 @@ import NavItem from './NavItem'
 import PropTypes from 'prop-types'
 import withUserData from '../../components/providers/withUserData'
 import injectSheet from 'react-jss'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
 
 const propTypes = {
   userData: PropTypes.object.isRequired
@@ -12,13 +14,13 @@ const styles = {
   root: {
     top: 0,
     left: 0,
-    right: 0,
+    right: 20,
     height: 50,
     backgroundColor: 'gray',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'left',
     fontFamily: 'Gloria Hallelujah',
     color: 'black'
   }
@@ -30,15 +32,19 @@ const Header = (props) => {
   console.log(props)
   return (
     <header>
-      <nav style={styles.root}>
-        <NavItem to='/'>Home</NavItem>
-        <NavItem to='/about'>About</NavItem>
-        <NavItem to='/jobs'>Jobs</NavItem>
-        { !props.userData.user ? <NavItem to='/signup'>Sign Up</NavItem> : <a onClick={props.userData.logoutUser}>Log Out</a> }
-        { props.userData.user ? <NavItem to='/profile'>Profile</NavItem>
-          : <NavItem to='/login'>Login</NavItem>
-        }
-      </nav>
+      <AppBar position='static' style={styles.root}>
+        <Toolbar>
+          <nav>
+            <NavItem to='/'>Home</NavItem>
+            <NavItem to='/about'>About</NavItem>
+            <NavItem to='/jobs'>Jobs</NavItem>
+            { !props.userData.user ? <NavItem to='/signup'>Sign Up</NavItem> : <a onClick={props.userData.logoutUser}>Log Out</a> }
+            { props.userData.user ? <NavItem to='/profile'>Profile</NavItem>
+              : <NavItem to='/login'>Login</NavItem>
+            }
+          </nav>
+        </Toolbar>
+      </AppBar>
     </header>
   )
 }
